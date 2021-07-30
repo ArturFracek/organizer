36378d14329a6ec58b59555138b8c481e42572a6
Artur Frącek <arturfracek7@gmail.com>
2021-07-30 18:40:03 +0200

    Fixed refreshing bug

--- client/src/components/LoginModal.vue
+++ client/src/components/LoginModal.vue
@@ -70,7 +70,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .login_container {
   display: flex;
--- client/src/components/RegisterModal.vue
+++ client/src/components/RegisterModal.vue
@@ -99,7 +99,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .registration_container {
   display: flex;
