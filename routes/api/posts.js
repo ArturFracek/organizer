764320530a01252c8248307387298eab5bfa2f1f
Artur Frącek <arturfracek7@gmail.com>
2021-08-17 15:16:46 +0200

    added aray receiving

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,12 +4,20 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-  res.send('hello')
+router.get('/', async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
 })
 
 // Add Post
 
 // Delete Post
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient
+  .connect('mongodb+srv://Setius:123>@cluster0.setab.mongodb.net/organiser_auth?retryWrites=true&w=majority',
+  { useNewUrlParser: true });
+  return client.db('organiser_auth').collection('posts')
+};
 
 module.exports = router;
