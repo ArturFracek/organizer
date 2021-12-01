d903619539b25e982d3250ef67dc4a5613566692
Artur Frącek <arturfracek7@gmail.com>
2021-12-01 15:03:52 +0100

    update

---
+++ tests/unit/specs/Activities.spec.js
@@ -0,0 +1,4 @@
+import Vuetify from "vuetify/lib";
+import {mount, createLocalVue} from '@vue/test-tils'
+import Vuex from 'vuex'
+import Activities from '@/compontents/Activities.vue'
--- tests/unit/specs/Goals.spec.js
+++ tests/unit/specs/Goals.spec.js
@@ -1,10 +1,18 @@
 import { createLocalVue, mount } from "@vue/test-utils";
-import Vue from "vue";
+import Vuex from "vuex";
 import Vuetify from "vuetify";
-import Goals from "@/components/Goals";
+import Goals from "../../../client/src/components/Goals.vue";
 import { jest } from "@jest/globals";
+import { cloneDeep } from "lodash";
+import Vue from "vue";
+
+Vue.use(Vuetify);
 
 const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.store(cloneDeep({}));
+
 const vuetify = new Vuetify();
 let wrapper;
 // const error = {
@@ -24,31 +32,34 @@ let wrapper;
 //     success: true,
 //   },
 // };
-
-beforeEach(() => {
-  wrapper = mount(Goals, {
-    localVue,
-    mocks: {
-      $store: {
-        getters: {
-          goals: "goals/goals",
+describe("test if goals are adding and displaying properly", () => {
+  beforeEach(() => {
+    wrapper = mount(Goals, {
+      localVue,
+      mocks: {
+        $store: {
+          getters: {
+            goals: "goals/goals",
+          },
         },
-        
       },
-    },
-    vuetify,
-    data() {
-      return {
-        title: "",
-        error: "Something went wrong, try again",
-      };
-    },
+      vuetify,
+      data() {
+        return {
+          title: "",
+          error: "Something went wrong, try again",
+        };
+      },
+    });
+    test("setup correctly", () => {
+      expect(true).toBe(true);
+    });
   });
-});
 
-afterEach(() => {
-  jest.resetAllMocks();
-  wrapper.destroy();
-});
+  afterEach(() => {
+    jest.resetAllMocks();
+    wrapper.destroy();
+  });
 
-jest.mock("axios");
+  jest.mock("axios");
+});
