ac0a2bd985953e31d96489420f91e8070044ca85
Artur Frącek <arturfracek7@gmail.com>
2021-12-06 12:16:14 +0100

    e2e test moved and unit test config repaired -installed vue-test-utils

--- tests/unit/specs/Activities.spec.js
+++ tests/unit/specs/Activities.spec.js
@@ -1,9 +1,9 @@
-import { mount, createLocalVue } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils"; //test-utils
 import Vuex from "vuex";
 import Activities from "@/components/Activities.vue";
 
-describe('test', () => {
-  it('do nothing', () => {
+describe("test", () => {
+  it("do nothing", () => {
     expect(1).toBe(1);
   });
 });
