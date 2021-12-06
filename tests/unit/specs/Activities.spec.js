13651b80833d13e8d77d06ab5ab59841fbb95126
Artur Frącek <arturfracek7@gmail.com>
2021-12-06 14:50:18 +0100

    app name update

--- tests/unit/specs/Activities.spec.js
+++ tests/unit/specs/Activities.spec.js
@@ -2,19 +2,14 @@ import { mount, createLocalVue } from "@vue/test-utils"; //test-utils
 import Vuex from "vuex";
 import Activities from "@/components/Activities.vue";
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
-
-
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe("should add activity, see if it appears and if input is cleared after submit", () => {
-
   it("add activity", () => {
-    const store = new Vuex.Store()
+    const store = new Vuex.Store({});
     const wrapper = mount(Activities, {
-      mocks: {
-
-      }
+      mocks: {},
     });
 
     wrapper
@@ -25,3 +20,4 @@ describe("should add activity, see if it appears and if input is cleared after s
     expect(wrapper.findAll("[data-test=activityShowModal]")).toHaveLength(1);
   });
 });
+123
