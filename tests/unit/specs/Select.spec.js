af9c403b7c14b396ddc772a0a8242a9f6948f426
Artur Frącek <arturfracek7@gmail.com>
2021-12-09 00:15:40 +0100

    unit test vuex and select added

--- tests/unit/specs/Select.spec.js
+++ tests/unit/specs/Select.spec.js
@@ -1,14 +1,63 @@
-import { mount, createLocalVue } from "@vue/test-utils"; //test-utils
+import { mount } from "@vue/test-utils"; //test-utils
 import Select from "@/components/Select.vue";
 
-describe("should add activity, see if it appears and if input is cleared after submit", () => {
-  it("add activity", () => {
-    const wrapper = mount(Select);
+const options = [
+  { label: "Label-A", key: "key-A", altKey: "alt-A" },
+  { label: "Label-B", key: "key-B", altKey: "alt-B" },
+  { label: "Label-C", key: "key-C", altKey: "alt-C" },
+];
 
-    wrapper
-      .get("[data-test='customSelectButton']")
-      .trigger('click')
+describe("Select", () => {
+  it("renders available options based on optionLabelKey props", () => {
+    let wrapper = mount(Select, {
+      propsData: {
+        options,
+        optionLabelKey: "label",
+      },
+    });
+    let renderedTexts = wrapper.findAll("option").wrappers.map((w) => w.text());
+    expect(renderedTexts).toEqual(options.map((o) => o.label));
 
-    expect(wrapper.findAll("[data-test='customSelectButton']")).toBe();
+    wrapper = mount(Select, {
+      propsData: {
+        options,
+        optionLabelKey: "altKey",
+      },
+    });
+    renderedTexts = wrapper.findAll("option").wrappers.map((w) => w.text());
+    expect(renderedTexts).toEqual(options.map((o) => o.altKey));
+  });
+
+  it("renders current value", () => {
+    const wrapper = mount(Select, {
+      propsData: {
+        options,
+        optionValueKey: "key",
+        optionLabelKey: "label",
+        value: "key-C",
+      },
+    });
+
+    expect(wrapper.vm.valueIndex).toBe(2);
+  });
+
+  it("emits input when slecting value", async () => {
+    const wrapper = mount(Select, {
+      propsData: {
+        options,
+        optionValueKey: "key",
+        optionLabelKey: "label",
+        value: "key-C",
+      },
+    });
+    expect(Object.keys(wrapper.emitted()).length).toBe(0);
+
+    wrapper.findAll("option").at(1).trigger("click");
+    await wrapper.vm.$nextTick();
+
+    console.log(wrapper.emitted());
+
+    expect(Object.keys(wrapper.emitted()).length).toBe(1);
+    expect(wrapper.emitted().input[0][0]).toBe("key-B");
   });
 });
