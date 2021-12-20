cb3468c282ef6319d1601b49064781c25d4c2c8a
Artur Frącek <arturfracek7@gmail.com>
2021-12-20 12:10:14 +0100

    Vuex activities unit tests done

--- tests/unit/specs/VuexActivities.spec.js
+++ tests/unit/specs/VuexActivities.spec.js
@@ -32,21 +32,54 @@ function getMockedDataForPostResponse(url) {
       throw new Error(`This test doest cover ${url} response`);
   }
 }
+function getMockedDataForDeleteResponse(url) {
+  switch (url) {
+    case "/activities/3":
+      return {};
+    default:
+      throw new Error(`This test doest cover ${url} response`);
+  }
+}
+function getMockedDataForPutResponse(url) {
+  switch (url) {
+    case "/activities/4":
+      return {};
+    default:
+      throw new Error(`This test doest cover ${url} response`);
+  }
+}
 
-describe("Activities", () => {
+describe("Activities Store Vuex", () => {
   const temp = {};
   // eslint-disable-next-line no-undef
   beforeEach(() => {
     temp.get = api.get;
     temp.post = api.post;
+    temp.delete = api.delete;
+    temp.put = api.put;
+
+    // eslint-disable-next-line no-undef
     api.post = jest.fn(function post(url, data) {
       return Promise.resolve({
         data: getMockedDataForPostResponse(url),
       });
     });
+    // eslint-disable-next-line no-undef
     api.get = jest.fn(function get(url) {
       return Promise.resolve({
         data: getMockedDataForGetResponse(url),
+      });
+    });
+    // eslint-disable-next-line no-undef
+    api.delete = jest.fn(function deletes(url, id) {
+      return Promise.resolve({
+        data: getMockedDataForDeleteResponse(url),
+      });
+    });
+    // eslint-disable-next-line no-undef
+    api.put = jest.fn(function put(url, data) {
+      return Promise.resolve({
+        data: getMockedDataForPutResponse(url),
       });
     });
   });
@@ -54,6 +87,8 @@ describe("Activities", () => {
   afterAll(() => {
     api.post = temp.post;
     api.get = temp.get;
+    api.delete = temp.delete;
+    api.put = temp.put;
   });
 
   it("adds activity", async () => {
@@ -63,12 +98,31 @@ describe("Activities", () => {
       title: "activity3",
       description: "test",
     });
-    console.log(api.post)
-  
+
     expect(api.post).toHaveBeenLastCalledWith("/activities/", {
       id: "3",
       title: "activity3",
       description: "test",
+    });
+  });
+  it("deletes activity", async () => {
+    expect(api.delete).not.toHaveBeenCalled();
+    await store.dispatch("activities/deleteActivity", {
+      id: "3",
+    });
+    expect(api.delete).toHaveBeenLastCalledWith("/activities/3");
+  });
+  it("updates activity", async () => {
+    expect(api.put).not.toHaveBeenCalled();
+    await store.dispatch("activities/updateActivity", {
+      _id: "4",
+      title: "activity4",
+      description: "test2",
+    });
+    expect(api.put).toHaveBeenLastCalledWith("/activities/4", {
+      _id: "4",
+      title: "activity4",
+      description: "test2",
     });
   });
 });
